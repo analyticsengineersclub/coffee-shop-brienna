1307b651e2f4b672f6de7ab6bf203eda5bff64af
Brienna Herold <brienna.herold@gmail.com>
2021-10-31 00:26:59 +0000

    Stitch visitor_ids so each customer has only one

---
+++ models/marts/fct_pageviews.sql
@@ -0,0 +1,34 @@
+with pageviews as (
+    select 
+        *
+    from `analytics-engineers-club.web_tracking.pageviews`
+    where customer_id is not null
+),
+
+one_visitor_id_per_customer as (
+    select 
+        customer_id, 
+        min(visitor_id) as visitor_id
+    from pageviews
+    group by 1
+),
+
+final as (
+    select 
+        pageviews.id, 
+        one_visitor_id_per_customer.visitor_id, 
+        pageviews.device_type, 
+        pageviews.timestamp, 
+        pageviews.page, 
+        pageviews.customer_id
+    from pageviews
+    left join one_visitor_id_per_customer   
+        on pageviews.customer_id = one_visitor_id_per_customer.customer_id
+    order by customer_id
+)
+
+select 
+    *
+from final
+where customer_id is not null
+order by 2 desc 
