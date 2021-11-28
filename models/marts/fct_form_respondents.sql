cf41ed61d3289fcd49f94cabeba557e1702729f2
Brienna Herold <brienna.herold@gmail.com>
2021-11-28 20:14:19 +0000

    Deal with data being split across two runs

--- models/marts/fct_form_respondents.sql
+++ models/marts/fct_form_respondents.sql
@@ -9,14 +9,18 @@ with events as (
     select * from {{ ref('stg_form_events') }}
 
     {% if is_incremental() %}
-    where timestamp >= ( 
-        select 
-            max(last_form_entry) 
-        from {{ this }} 
-        -- we don't want to use a ref, as this creates a cyclic dependency
-        -- we also don't want to hard code it, e.g. dbt_brienna.fct_form_respondents, 
-        -- cuz then it wouldn't work if someone else is using it
+    where github_username in ( -- ensures that we find the last event associated with the respondent
+        select distinct github_username from {{ source('advanced_dbt_examples', 'form_events') }}
+        where timestamp >= ( 
+            select 
+                max(last_form_entry) 
+            from {{ this }} 
+            -- we don't want to use a ref, as this creates a cyclic dependency
+            -- we also don't want to hard code it, e.g. dbt_brienna.fct_form_respondents, 
+            -- cuz then it wouldn't work if someone else is using the model
+        )
     )
+    
     {% endif %}
 ),
 
